8bfe5fa4b3b979f3c03ad7d62c982f0ca97a3290
MCWXT <2775997367@qq.com>
2024-06-28 23:40:17 +0800

    update

--- js/script.js
+++ js/script.js
@@ -15,8 +15,24 @@ imports("footer");
 
 const dom = {
   menu: $(".menu>div"),
-  loading: $("loading")
+  loading: $("loading"),
+  nav: $(".nav"),
+  naver: $(".naver")
 }
+dom.naver[0].style.height = window.getComputedStyle(dom.nav[0]).height;
+const scroll = () => {
+  const scrollTop = $(window).scrollTop();
+  if (scrollTop >= parseFloat(window.getComputedStyle(dom.nav[0]).height)) {
+    dom.nav.css({
+      "background-color": "#FFFFFFCC",
+    });
+  } else {
+    dom.nav.css({
+      "background-color": "white",
+    });
+  }
+}
+$(window).on('scroll', () => scroll());
 var display = false;
 const isDisplay = () => {
   if (dom.menu[0].style.animationName == "displayMenu" || null) {
