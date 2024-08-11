d3627c88ae821741c92c0892dc7c6f7309094906
MCWXT <2775997367@qq.com>
2024-08-11 20:23:29 +0800

    更新论坛系统

--- script/messege.js
+++ script/messege.js
@@ -1,5 +1,10 @@
 const message = JSON.parse($.ajax({
-  url: 'https://api.github.com/repos/MCWXT/mcwxt.github.io/issues/' + getQueryString('number'),
+  url: 'https://api.github.com/repos/MCWXT/mcwxt.github.io/issues/' + (getQueryString('number') || 3),
+  dataType: 'json',
+  async: false
+}).responseText);
+const comments = JSON.parse($.ajax({
+  url: message.comments_url,
   dataType: 'json',
   async: false
 }).responseText);
@@ -10,6 +15,19 @@ message.MDHTML = markdown.toHTML(message.body);
 tao.replace({
   message: message
 });
-$('img').prop('src',(index,currentvalue) => {
+$('#md img').prop('src',(index,currentvalue) => {
   return 'https://proxy.mcwxt.top/' + currentvalue;
 });
+comments.map(comment => {
+  comment.author = comment.user.login;
+  comment.authorURI = comment.user.html_url;
+  comment.authorAvatar = comment.user.avatar_url;
+});
+tao.for({
+  comments: comments
+});
+tao.replace({
+  messageUrl: {
+    messageUrl: message.html_url
+  }
+})
