82479c0ca027b1af4abb9a79c561a35be052a8bc
MCWXT <2775997367@qq.com>
2024-10-01 15:18:37 +0800

    update githubCallback.js

--- script/githubCallback.js
+++ script/githubCallback.js
@@ -5,10 +5,16 @@ createApp({
     const isSuccessful = ref(false);
     if (getQueryString('code'), getQueryString('state') == 'MCWXT') {
       const access_token = $.parseJSON($.ajax({
-      url: 'https://github.com/login/oauth/access_token?client_id=Iv23lieAt4NOqGN5GqZI&client_secret=0ca4b19709671aee0011545baf857283cee944a4&code=' + getQueryString('code'),
-      dataType: "json",
-      async: false
-    }).responseText).access_token;
+        url: 'https://github.com/login/oauth/access_token',
+        type: 'POST',
+        dataType: "json",
+        data: {
+          client_id: 'Iv23lieAt4NOqGN5GqZI',
+          client_secret: '0ca4b19709671aee0011545baf857283cee944a4',
+          code: getQueryString('code')
+        },
+        async: false
+      }).responseText).access_token;
       window.localStorage.setItem('access_token', access_token);
       isSuccessful.value = true;
       setTimeout(() => {
