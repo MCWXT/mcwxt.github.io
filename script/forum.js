da5607cb67480c7c3960a8fbcc04b088a94515bb
MCWXT <2775997367@qq.com>
2024-10-28 22:21:24 +0800

    forum大更新

--- script/forum.js
+++ script/forum.js
@@ -1,19 +1,19 @@
-import { github } from 'githubApi';
 import { createApp, ref } from 'vue';
 import { tao } from 'tao';
 createApp({
   setup() {
     const messages = JSON.parse($.ajax({
-      url: github.issuesApi,
+      url: 'https://halo.mcwxt.top/apis/api.content.halo.run/v1alpha1/posts',
       dataType: 'json',
       async: false
-    }).responseText);
+    }).responseText).items;
+    const timeFormat = (str) => {
+      return str.slice(0, str.indexOf('T'));
+    }
     return {
       tao,
-      carouselImg: [{ imgSrc: '//i2.hdslb.com/bfs/archive/c96b635dcadbb30d5aa73a9efc532e3bb9d40d6e.jpg' },
-      { imgSrc: '//i0.hdslb.com/bfs/archive/26e4fb5b6c57d75e945fbd06f334c5412c04aa17.png' },
-      { imgSrc: '//i2.hdslb.com/bfs/archive/ffe2a3758e07429f89294316aa548bad79f06f80.png' }],
-      messages
+      messages,
+      timeFormat
     }
   }
 }).mount('#app');
