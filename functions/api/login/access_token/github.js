577a6e69e8c9ae1e789f4d6021516ac68101b944
MCWXT <2775997367@qq.com>
2024-12-25 22:11:35 +0800

    update github.js

--- functions/api/login/access_token/github.js
+++ functions/api/login/access_token/github.js
@@ -1,14 +1,14 @@
 export async function onRequestPost(context) {
   const request = context.request;
   const data = JSON.parse(request.body);
-  const githubRequest = new Request('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    body: {
-      code: data.code,
-      client_id: 'Iv23lieAt4NOqGN5GqZI',
-      client_secret: context.env.githubAppClientSecret
-    }
-  });
-  const githubResponse = await fetch(githubRequest);
-  return new Response(githubResponse.body);
+  // const githubRequest = new Request('https://github.com/login/oauth/access_token', {
+  //   method: 'POST',
+  //   body: {
+  //     code: data.code,
+  //     client_id: 'Iv23lieAt4NOqGN5GqZI',
+  //     client_secret: context.env.githubAppClientSecret
+  //   }
+  // });
+  // const githubResponse = await fetch(githubRequest);
+  return new Response();
 }
