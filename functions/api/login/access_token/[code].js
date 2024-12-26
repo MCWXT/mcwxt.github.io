6aed4f6ea385d0a7e66785b0c30655df9aabd910
MCWXT <2775997367@qq.com>
2024-12-26 21:03:05 +0800

    update [code].js

--- functions/api/login/access_token/[code].js
+++ functions/api/login/access_token/[code].js
@@ -1,11 +1,12 @@
 export async function onRequestPost(context) {
-  const request = new Request('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    data: {
+  const data = {
       code: context.params.code,
       client_id: 'Iv23lieAt4NOqGN5GqZI',
       client_secret: context.env.githubAppClientSecret
     }
+  const request = new Request('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    body: JSON.stringify(data)
   });
   const response = await fetch(request);
   return new Response(response.body);
