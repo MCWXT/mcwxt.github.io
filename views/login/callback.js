1b17ea4690a1d9d271d95733c497e1a28fde4940
MCWXT <2775997367@qq.com>
2024-12-28 18:56:50 +0800

    update callback.js

--- views/login/callback.js
+++ views/login/callback.js
@@ -5,7 +5,11 @@ import { Axios } from '/cdn_modules/axios@1.7.8/esm/axios.min.js';
 export default {
   setup() {
     const route = useRoute();
-    const axios = new Axios({});
+    const axios = new Axios({
+      header: {
+        Accept: 'application/json'
+      }
+    });
     axios.post('/api/login/access_token/' + route.query.code).then((response) => {
       cache.set('github_access', response.data);
     });
