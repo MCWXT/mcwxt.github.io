9d87de7fdb6c309a8fc5dc40ee8b1ab9a6827490
MCWXT <2775997367@qq.com>
2024-12-28 18:31:09 +0800

    update callback.js

--- views/login/callback.js
+++ views/login/callback.js
@@ -1,20 +1,14 @@
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getTemplate } from 'tao';
+import '/cdn_modules/qs@6.13.1/qs.min.js';
 import { Axios } from '/cdn_modules/axios@1.7.8/esm/axios.min.js';
 export default {
   setup() {
     const route = useRoute();
-    const axios = new Axios({
-      transformResponse: (data) => {
-        if (typeof data != 'object') {
-          return JSON.parse(data);
-        }
-        return data;
-      }
-    });
+    const axios = new Axios({});
     axios.post('/api/login/access_token/' + route.query.code).then((response) => {
-      cache.set('github_access', response.data);
+      cache.set('github_access', Qs.parse(response.data));
     });
     return {
       
