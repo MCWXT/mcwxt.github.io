a0b0fe7260ef137b0be872c75676f974d36004d2
MCWXT <2775997367@qq.com>
2024-12-28 19:04:06 +0800

    update

--- functions/api/login/access_token/[code].js
+++ functions/api/login/access_token/[code].js
@@ -1,6 +1,9 @@
 export async function onRequestPost(context) {
+  const headers = new Headers();
+  headers.append('Accept', 'application/json');
   const request = new Request(`https://github.com/login/oauth/access_token?code=${context.params.code}&client_id=Iv23lieAt4NOqGN5GqZI&client_secret=${context.env.githubAppClientSecret}`, {
-    method: 'POST'
+    method: 'POST',
+    headers
   });
   const response = await fetch(request);
   return new Response(response.body);
--- views/login/callback.js
+++ views/login/callback.js
@@ -5,11 +5,7 @@ import { Axios } from '/cdn_modules/axios@1.7.8/esm/axios.min.js';
 export default {
   setup() {
     const route = useRoute();
-    const axios = new Axios({
-      header: {
-        Accept: 'application/json'
-      }
-    });
+    const axios = new Axios({});
     axios.post('/api/login/access_token/' + route.query.code).then((response) => {
       cache.set('github_access', response.data);
     });
